e88bfdfe96f8bd118318edb50ac29f31590fa644
Saksham Agrahari <agrahari0899@gmail.com>
2025-09-14 03:49:37 +0530

    feat: Creating Authentication Middleware

---
+++ backend/middlewares/isAuth.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken"
+
+const isAuth = async (req, res, next) => {
+    try {
+        const token = req.cookie.token;
+        if (!token) {
+            return res.status(400).json({ message: "Token not found" })
+        }
+
+        // decode token
+        const decodeToken = jwt.verify(token, process.env.JWT_SECRET)
+        if (!decodeToken) {
+            return res.status(400).json({ message: "Token not verify" })
+        }
+
+        // get and store user id
+        req.userId = decodeToken.userId
+        next()
+    } catch (error) {
+        return res.status(500).json({ message: "Authentication error" })
+    }
+}
+
+export default isAuth
