7df3cf5dd423c15d26656fb602cd65ce648697f4
Saksham Agrahari <agrahari0899@gmail.com>
2025-09-15 18:38:23 +0530

    feat: Create Shop and Item model

---
+++ backend/models/item.model.js
@@ -0,0 +1,63 @@
+import mongoose from "mongoose";
+
+const itemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    shop: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Shop"
+    },
+    category: {
+        type: String,
+        enum: [
+            "Snacks",
+            "Pizza",
+            "Burgers",
+            "Sandwiches",
+            "Rolls",
+            "Biryani",
+            "North Indian",
+            "South Indian",
+            "Chinese",
+            "Momos",
+            "Pasta",
+            "Salads",
+            "Desserts",
+            "Ice Cream",
+            "Cakes",
+            "Beverages",
+            "Tea & Coffee",
+            "Juices",
+            "Shakes",
+            "Chicken",
+            "Vegan",
+            "Pure Veg",
+            "Street Food",
+            "Healthy Food",
+            "Breakfast",
+            "Combo Meals",
+            "Other"
+        ],
+        required: true
+    },
+    price: {
+        type: Number,
+        min: 0,
+        required: true
+    },
+    foodType: {
+        type: String,
+        enum: ["veg", "non veg"],
+        required: true
+    }
+}, { timestamps: true })
+
+const Item = mongoose.model("Item", itemSchema)
+
+export default Item
