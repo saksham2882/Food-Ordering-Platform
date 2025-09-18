32f9cd34fd6dc310f9fd85cb1a02728b351ebade
Saksham Agrahari <agrahari0899@gmail.com>
2025-09-18 10:37:04 +0530

    feat: Create Add Shop for Owner Dashboard

--- frontend/src/components/OwnerDashboard.jsx
+++ frontend/src/components/OwnerDashboard.jsx
@@ -1,9 +1,12 @@
-import { useSelector } from "react-redux"
-import Navbar from "./Navbar"
-import { FaUtensils } from "react-icons/fa6"
+import { useSelector } from "react-redux";
+import Navbar from "./Navbar";
+import { FaUtensils } from "react-icons/fa6";
+import { FaPen } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 const OwnerDashboard = () => {
-  const { myShopData } = useSelector((state) => state.owner)
+  const { myShopData } = useSelector((state) => state.owner);
+  const navigate = useNavigate();
 
   return (
     <div className="w-full min-h-screen bg-bg flex flex-col items-center">
@@ -15,14 +18,85 @@ const OwnerDashboard = () => {
           <div className="w-full max-w-md bg-white shadow-lg rounded-2xl p-6 border border-gray-200 hover:shadow-xl transition-shadow duration-300">
             <div className="flex flex-col items-center text-center">
               <FaUtensils className="text-primary w-16 h-16 sm:w-20 sm:h-20 mb-4" />
-              <h2 className="text-xl sm:text-2xl font-bold text-gray-800 mb-2">Add Your Restaurant</h2>
-              <p className="text-gray-600 mb-4 text-sm sm:text-base">Join our food delivery and reach thousands of hungry customers every day.</p>
+              <h2 className="text-xl sm:text-2xl font-bold text-gray-800 mb-2">
+                Add Your Restaurant
+              </h2>
+              <p className="text-gray-600 mb-4 text-sm sm:text-base">
+                Join our food delivery and reach thousands of hungry customers
+                every day.
+              </p>
 
-              <button className="bg-primary text-white px-5 sm:px-6 py-2 rounded-full font-medium shadow-md hover:bg-orange-700 hover:shadow-lg transition-colors duration-200 cursor-pointer">
+              <button
+                className="bg-primary text-white px-5 sm:px-6 py-2 rounded-full font-medium shadow-md hover:bg-orange-700 hover:shadow-lg transition-colors duration-200 cursor-pointer"
+                onClick={() => navigate("/create-edit-shop")}
+              >
                 Get Started
               </button>
             </div>
           </div>
+        </div>
+      )}
+
+      {myShopData && (
+        <div className="w-full flex flex-col items-center gap-6 px-4 sm:px-6">
+          <h1 className="text-2xl sm:text-3xl text-gray-600 flex items-center gap-3 mt-8 text-center text-shadow-sm text-shadow-gray-300 font-medium">
+            <FaUtensils className="text-primary w-14 h-14" />
+            <span>
+              Welcome to{" "}
+              <strong className="text-primary">{myShopData.name}</strong>
+            </span>
+          </h1>
+
+          {/* --------------- Shop info ------------- */}
+          <div className="bg-white shadow-lg rounded-xl overflow-hidden border border-orange-100 hover:shadow-xl transition-all duration-300 w-full max-w-3xl relative">
+            {/* ---------- Edit Shop button -------- */}
+            <div
+              className="absolute top-4 right-4 bg-primary text-white p-2 rounded-full shadow-md hover:bg-orange-600 transition-colors cursor-pointer"
+              onClick={() => navigate("/create-edit-shop")}
+            >
+              <FaPen size={20} />
+            </div>
+
+            <img
+              src={myShopData.image}
+              alt={myShopData.name}
+              className="w-full h-48 sm:h-64 object-cover"
+            />
+
+            <div className="p-4 sm:p-6">
+              <h1 className="text-xl sm:text-2xl font-bold text-gray-800 mb-2">
+                {myShopData.name}
+              </h1>
+              <p className="text-gray-500">
+                {myShopData.city}, {myShopData.state}
+              </p>
+              <p className="text-gray-500 mb-4">{myShopData.address}</p>
+            </div>
+          </div>
+
+          {/* --------------- Add food item div when no items presents ------------ */}
+          {myShopData.items.length == 0 && (
+            <div className="flex justify-center items-center p-4 sm:p-6">
+              <div className="w-full max-w-md bg-white shadow-lg rounded-2xl p-6 border border-gray-200 hover:shadow-xl transition-shadow duration-300">
+                <div className="flex flex-col items-center text-center">
+                  <FaUtensils className="text-primary w-16 h-16 sm:w-20 sm:h-20 mb-4" />
+                  <h2 className="text-xl sm:text-2xl font-bold text-gray-800 mb-2">
+                    Add Your Food Item
+                  </h2>
+                  <p className="text-gray-600 mb-4 text-sm sm:text-base">
+                    Share your delicious creations with our customers by adding them to the menu.
+                  </p>
+
+                  <button
+                    className="bg-primary text-white px-5 sm:px-6 py-2 rounded-full font-medium shadow-md hover:bg-orange-700 hover:shadow-lg transition-colors duration-200 cursor-pointer"
+                    onClick={() => navigate("/add")}
+                  >
+                    Add Food
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       )}
     </div>
---
+++ frontend/src/pages/CreateEditShop.jsx
@@ -0,0 +1,179 @@
+import { IoIosArrowRoundBack } from "react-icons/io";
+import { FaUtensils } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+import { SERVER_URL } from "../App";
+import { setMyShopData } from "../redux/ownerSlice";
+import { toast } from "sonner";
+import { ClipLoader } from "react-spinners";
+
+const CreateEditShop = () => {
+  const navigate = useNavigate();
+  const { myShopData } = useSelector((state) => state.owner);
+  const { currentCity, currentState, currentAddress } = useSelector((state) => state.user);
+  const dispatch = useDispatch()
+
+  const [name, setName] = useState(myShopData?.name || "");
+  const [address, setAddress] = useState(myShopData?.address || currentAddress);
+  const [city, setCity] = useState(myShopData?.city || currentCity);
+  const [state, setState] = useState(myShopData?.state || currentState);
+  const [imagePreview, setImagePreview] = useState(myShopData?.image || null);    // frontend
+  const [uploadImage, setUploadImage] = useState(null);                           // backend
+  const [loading, setLoading] = useState(false);
+
+  const handleImage = (e) => {
+    const file = e.target.files[0];
+    setUploadImage(file);
+    setImagePreview(URL.createObjectURL(file));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const formData = new FormData();
+      formData.append("name", name);
+      formData.append("city", city);
+      formData.append("state", state);
+      formData.append("address", address);
+      if (uploadImage) {
+        formData.append("image", uploadImage);
+      }
+
+      const res = await axios.post(`${SERVER_URL}/api/shop/create-edit`, formData, {
+        withCredentials: true,
+      });
+
+      dispatch(setMyShopData(res.data))
+      toast.success(!myShopData ? "Shop Created Successfully" : "Shop Updated Successfully")
+
+    } catch (error) {
+      toast.error(error?.response?.data?.message || "Something went wrong. Please try again.")
+    } finally {
+      setLoading(false)
+    }
+  };
+
+  return (
+    <div className="flex justify-center flex-col items-center p-6 bg-gradient-to-br from-orange-50 relative to-white min-h-screen">
+      <div
+        className="absolute top-[20px] left-[20px] z-[10] mb-[10px]"
+        onClick={() => navigate("/")}
+      >
+        <IoIosArrowRoundBack
+          size={35}
+          className="text-primary cursor-pointer"
+        />
+      </div>
+
+      <div className="max-w-lg w-full bg-white shadow-xl rounded-2xl p-8 border-orange-100">
+        {/* ---------- Add or Edit Shop Icon --------- */}
+        <div className="flex flex-col items-center mb-6">
+          <div className="bg-orange-100 p-4 rounded-full mb-4">
+            <FaUtensils className="text-primary w-16 h-16" />
+          </div>
+
+          <div className="text-3xl font-extrabold text-gray-900">
+            {myShopData ? "Edit Shop" : "Add Shop"}
+          </div>
+        </div>
+
+        {/* ----------- Form ----------- */}
+        <form className="space-y-5" onSubmit={handleSubmit}>
+          {/* -------- Name -------- */}
+          <div>
+            <label className="block text-sm font-medium text-gray-700 mb-1">
+              Name
+            </label>
+            <input
+              type="text"
+              placeholder="Enter shop name"
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+            />
+          </div>
+
+          {/* ---------- image --------- */}
+          <div>
+            <label className="block text-sm font-medium text-gray-700 mb-1">
+              Shop Image
+            </label>
+            <input
+              type="file"
+              accept="image/*"
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500"
+              onChange={handleImage}
+            />
+            {imagePreview && (
+              <div className="mt-4">
+                <img
+                  src={imagePreview}
+                  alt=""
+                  className="w-full h-48 object-cover rounded-lg border"
+                />
+              </div>
+            )}
+          </div>
+
+          {/* ---------- Address --------- */}
+          <div className="flex grid-cols-1 md:grid-cols-2 gap-4">
+            {/* --------- City --------- */}
+            <div>
+              <label className="block text-sm font-medium text-gray-700 mb-1">
+                City
+              </label>
+              <input
+                type="text"
+                placeholder="Enter city"
+                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500"
+                onChange={(e) => setCity(e.target.value)}
+                value={city}
+              />
+            </div>
+
+            {/* -------- State ----------- */}
+            <div>
+              <label className="block text-sm font-medium text-gray-700 mb-1">
+                State
+              </label>
+              <input
+                type="text"
+                placeholder="Enter state"
+                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500"
+                onChange={(e) => setState(e.target.value)}
+                value={state}
+              />
+            </div>
+          </div>
+
+          {/* ------------- Address ------------- */}
+          <div>
+            <label className="block text-sm font-medium text-gray-700 mb-1">
+              Address
+            </label>
+            <input
+              type="text"
+              placeholder="Enter shop address"
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500"
+              onChange={(e) => setAddress(e.target.value)}
+              value={address}
+            />
+          </div>
+
+          {/* ------------ Save Button ------------ */}
+          <button
+            className="w-full bg-primary text-white px-6 py-3 rounded-lg font-semibold shadow-md hover:bg-orange-600 hover:shadow-lg transition-all cursor-pointer"
+            disabled={loading}
+          >
+            {loading ? <ClipLoader size={20} color="white" /> : "Save"}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default CreateEditShop;
