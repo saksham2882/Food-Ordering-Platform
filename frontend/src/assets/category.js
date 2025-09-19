f47eca4f4073770785bf8999e43b0adbdc5282a2
Saksham Agrahari <agrahari0899@gmail.com>
2025-09-19 19:16:47 +0530

    feat: Create Food Category Slider in  User Dashboard

---
+++ frontend/src/assets/category.js
@@ -0,0 +1,45 @@
+import menu_1 from './Appetizers.jpg'
+import menu_2 from './Beverages.jpg'
+import menu_3 from './Biryani.jpg'
+import menu_4 from './Breads.jpeg'
+import menu_5 from './Breakfast.jpg'
+import menu_6 from './Burgers.jpg'
+import menu_7 from './Cake.webp'
+import menu_8 from './Chiness_food.jpg'
+import menu_9 from './Dessert.jpeg'
+import menu_10 from './Fast-food.jpeg'
+import menu_11 from './Healthy_food.jpg'
+import menu_12 from './Indian_food.jpg'
+import menu_13 from './Italian_food.webp'
+import menu_14 from './Juices.jpeg'
+import menu_15 from './Noodles.webp'
+
+export const categories = [
+    { category: "Snacks", image: menu_1 },
+    { category: "Pizza", image: menu_10 },
+    { category: "Burgers", image: menu_6 },
+    { category: "Sandwiches", image: menu_10 },
+    { category: "Rolls", image: menu_10 },
+    { category: "Biryani", image: menu_3 },
+    { category: "North Indian", image: menu_12 },
+    { category: "South Indian", image: menu_12 },
+    { category: "Chinese", image: menu_8 },
+    { category: "Momos", image: menu_8 },
+    { category: "Pasta", image: menu_13 },
+    { category: "Salads", image: menu_11 },
+    { category: "Desserts", image: menu_9 },
+    { category: "Ice Cream", image: menu_9 },
+    { category: "Cakes", image: menu_7 },
+    { category: "Beverages", image: menu_2 },
+    { category: "Tea & Coffee", image: menu_2 },
+    { category: "Juices", image: menu_14 },
+    { category: "Shakes", image: menu_14 },
+    { category: "Chicken", image: menu_12 },
+    { category: "Vegan", image: menu_11 },
+    { category: "Pure Veg", image: menu_11 },
+    { category: "Street Food", image: menu_10 },
+    { category: "Healthy Food", image: menu_11 },
+    { category: "Breakfast", image: menu_5 },
+    { category: "Combo Meals", image: menu_1 },
+    { category: "Other", image: menu_15 },
+]
--- frontend/src/components/UserDashboard.jsx
+++ frontend/src/components/UserDashboard.jsx
@@ -1,11 +1,116 @@
-import Navbar from "./Navbar"
+import { useEffect, useRef, useState } from "react";
+import { categories } from "../assets/category";
+import CategoryCard from "./CategoryCard";
+import Navbar from "./Navbar";
+import { FaCircleChevronLeft, FaCircleChevronRight } from "react-icons/fa6";
+import { useSelector } from "react-redux";
 
 const UserDashboard = () => {
-  return (
-    <div>
-        <Navbar />
-    </div>
-  )
-}
+  const { currentCity } = useSelector((state) => state.user);
+  const cateScrollRef = useRef();
+  const [showLeftCategoryBtn, setShowLeftCategoryBtn] = useState(false);
+  const [showRightCategoryBtn, setShowRightCategoryBtn] = useState(false);
 
-export default UserDashboard
+  const updateButton = (ref, setLeftBtn, setRightBtn) => {
+    const element = ref.current;
+    if (element) {
+      setLeftBtn(element.scrollLeft > 0);
+      setRightBtn(
+        element.scrollLeft + element.clientWidth < element.scrollWidth
+      );
+      // console.log("client width: ", element.clientWidth)
+      // console.log("scroll width: ", element.scrollWidth)
+      // console.log("scroll left: ", element.scrollLeft)
+    }
+  };
+
+  const scrollHandler = (ref, direction) => {
+    if (ref.current) {
+      ref.current.scrollBy({
+        left: direction == "left" ? -200 : 200,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (cateScrollRef.current) {
+      updateButton(
+        cateScrollRef,
+        setShowLeftCategoryBtn,
+        setShowRightCategoryBtn
+      );
+      cateScrollRef.current.addEventListener("scroll", () => {
+        updateButton(
+          cateScrollRef,
+          setShowLeftCategoryBtn,
+          setShowRightCategoryBtn
+        );
+      });
+    }
+
+    return () =>
+      cateScrollRef.current.removeEventListener("scroll", () => {
+        updateButton(
+          cateScrollRef,
+          setShowLeftCategoryBtn,
+          setShowRightCategoryBtn
+        );
+      });
+  }, [categories]);
+
+  return (
+    <div className="w-screen min-h-screen flex flex-col gap-5 items-center bg-bg overflow-y-auto">
+      <Navbar />
+
+      {/* ------------- Food Category ------------- */}
+      <div className="w-full max-w-6xl flex flex-col gap-5 items-start p-[10px]">
+        <h1 className="text-gray-700 text-xl sm:text-2xl">
+          Inspiration for your first order
+        </h1>
+
+        {/* ----------- Food Categories Slider --------------- */}
+        <div className="w-full relative">
+          {/* -------------- Left Button ------------- */}
+          {showLeftCategoryBtn && (
+            <button
+              className="absolute left-1 top-1/2 -translate-y-1/2 bg-primary text-white p-2 rounded-full shadow-lg hover:bg-hover z-10 cursor-pointer"
+              onClick={() => scrollHandler(cateScrollRef, "left")}
+            >
+              <FaCircleChevronLeft size={25} />
+            </button>
+          )}
+
+          {/* -------------- Categories --------------- */}
+          <div
+            className="w-full flex overflow-x-auto gap-4 pb-2"
+            ref={cateScrollRef}
+          >
+            {categories.map((cate, index) => (
+              <CategoryCard data={cate} key={index} />
+            ))}
+          </div>
+
+          {/* -------------- Right Button ------------- */}
+          {showRightCategoryBtn && (
+            <button
+              className="absolute right-1 top-1/2 -translate-y-1/2 bg-primary text-white p-2 rounded-full shadow-lg hover:bg-hover z-10 cursor-pointer"
+              onClick={() => scrollHandler(cateScrollRef, "right")}
+            >
+              <FaCircleChevronRight size={25} />
+            </button>
+          )}
+        </div>
+      </div>
+
+      {/* ------------- Shops by Location -------------- */}
+      <div className="w-full max-w-6xl flex flex-col gap-5 items-start p-[10px]">
+        <h1 className="text-gray-700 text-xl sm:text-2xl">
+          Best Shop in {currentCity}
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default UserDashboard;
